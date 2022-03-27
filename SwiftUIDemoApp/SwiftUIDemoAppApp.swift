2cd5646c5457e679bda7b92b29bf4ea12c6c6407
MarkPushkar92 <mark.pushkar92@gmail.com>
2022-03-27 16:22:05 +1000

    Initial Commit

---
+++ SwiftUIDemoApp/SwiftUIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoAppApp.swift
+//  SwiftUIDemoApp
+//
+//  Created by Марк Пушкарь on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
